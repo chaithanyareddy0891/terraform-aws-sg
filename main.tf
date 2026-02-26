822c2503c0bcaec3c18546332296db502e2a545b
chaithanyareddy <chaithanya.reddy0891@gmail.com>
2026-02-26 09:02:41 +0530

    adding description

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "main" {
   name        = local.sg_final_name
-  description = "Allow TLS inbound traffic and all outbound traffic"
+  description = "Allow SG inbound traffic and outbound traffic"
   vpc_id      = var.vpc_id
 
   egress {
